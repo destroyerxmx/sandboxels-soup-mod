d12c9e9b67c44553482ba12aa5a56c5edb3946df
destroyerxmx <134353830+destroyerxmx@users.noreply.github.com>
2023-05-22 23:41:04 -0400

    Update soup.js

--- soup.js
+++ soup.js
@@ -1,8 +1,16 @@
-elements.soup = {
+/*elements.soup = {
   color: "#FFFF4D"
   behavior: behaviors.LIQUID,
   category: "liquids",
   viscocity: 74000,
   state: "liquid",
   density: 720
+};*/
+elements.mayoo = {
+    color: "#ffffff",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
 };
